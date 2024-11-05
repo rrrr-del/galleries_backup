8ab5796a8b1db48f86a5fcaa7ca82f2e700fc578
ali <alirahimzade200444@gmail.com>
2024-11-05 21:02:26 +0330

    issue of name and icon chat part moving solved and a place for name of gallery created(but it has been falied to be shown yet)

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -19,44 +19,62 @@ const Gallary: React.FC<Props> = ({ image_url, descryption, name }) => {
   };
 
   return (
-    <Card
-      sx={{ width: 400, height: 400, backgroundColor: '#e0e0e0' }}
+<Card
+      sx={{ width: 400, height: 450, backgroundColor: '#e0e0e0' }}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-    >
-      <Box sx={{ width: 400, height: 400, display: 'flex', flexDirection: 'column' }}>
+>
+    <Box sx={{ width: 400, height: 450, display: 'flex', flexDirection: 'column' }}>
         {hover ? (
-          <Box sx={{ width: 400, height: 350, overflow: 'hidden' }}>
-            <Typography variant="h6" sx={{ textAlign: 'justify', padding: 3 }}>
-              {truncateString(descryption)}
-            </Typography>
+            <Box sx={{ width: 400, height: 350, overflow: 'hidden' }}>
+                <Typography variant="h6" sx={{ textAlign: 'justify', padding: 3 }}>
+                {truncateString(descryption)}
+                </Typography>
           </Box>
         ) : (
           <CardMedia
+            
             component="img"
             image={image_url}
             alt="Gallery Image"
             sx={{
-              width: '100%',
+              overflow: 'hidden',
+              width: 400 ,
               height: 350,
               objectFit: 'fill',
             }}
           />
         )}
-        <Box
-          sx={{
-            width: 400,
-            height: 50,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderTop: '1px solid #424242',
-          }}
-        >
-          <FaRocketchat style={{ fontSize: '24px' }} />
-        </Box>
-      </Box>
-    </Card>
+
+            <Box 
+                sx={{
+                width: 400,
+                height: 50,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderTop: '1px solid #424242',
+                }}
+            >
+                <Typography variant="h5" sx ={{overflow: 'hidden'}}>            
+                    {name}
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    width: 400,
+                    height: 50,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderTop: '1px solid #424242',
+                }}
+            >
+                <FaRocketchat style={{ fontSize: '24px' }} />
+            </Box>
+        
+    </Box>
+</Card>
   );
 };
 

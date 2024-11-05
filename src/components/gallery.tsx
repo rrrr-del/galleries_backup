c5115d1be38bad54e7b5c096dcee700fe9ba6ce3
ali <alirahimzade200444@gmail.com>
2024-11-05 20:28:45 +0330

    the icon chat was separated from image

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Card, CardMedia, CardContent, Typography, Box, Button } from '@mui/material';
+import { Card, CardMedia, Typography, Box } from '@mui/material';
 import { FaRocketchat } from 'react-icons/fa6';
 
 interface Props {
   image_url: string;
   descryption: string;
+  name: string;
 }
 
-const Gallary: React.FC<Props> = ({ image_url, descryption }) => {
+const Gallary: React.FC<Props> = ({ image_url, descryption, name }) => {
   const [hover, setHover] = useState(false);
 
   const truncateString = (input: string): string => {
@@ -23,40 +24,38 @@ const Gallary: React.FC<Props> = ({ image_url, descryption }) => {
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-      {hover ? (
-        <Box sx={{ width: 400, height: 400, display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ width: 400, height: 350, display: 'flex' }}>
+      <Box sx={{ width: 400, height: 400, display: 'flex', flexDirection: 'column' }}>
+        {hover ? (
+          <Box sx={{ width: 400, height: 350, overflow: 'hidden' }}>
             <Typography variant="h6" sx={{ textAlign: 'justify', padding: 3 }}>
               {truncateString(descryption)}
             </Typography>
           </Box>
-          <Box
-            sx={{
-              width: 400,
-              height: 50,
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderTop: '1px solid #424242',
-            }}
-          >
-            <FaRocketchat style={{ fontSize: '24px' }} />
-          </Box>
-        </Box>
-      ) : (
-        <Box sx={{ width: 400, height: 400 }}>
+        ) : (
           <CardMedia
             component="img"
             image={image_url}
             alt="Gallery Image"
             sx={{
               width: '100%',
-              height: '100%',
-              objectFit: 'fill', 
+              height: 350,
+              objectFit: 'fill',
             }}
           />
+        )}
+        <Box
+          sx={{
+            width: 400,
+            height: 50,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderTop: '1px solid #424242',
+          }}
+        >
+          <FaRocketchat style={{ fontSize: '24px' }} />
         </Box>
-      )}
+      </Box>
     </Card>
   );
 };
